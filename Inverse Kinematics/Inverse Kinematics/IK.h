40a9f850e98b497813df51c51da3775593f0d199
xxie5 <xiaowen.xie93@gmail.com>
2013-12-04 10:32:50 -0800

    Update IK.h

--- Inverse Kinematics/Inverse Kinematics/IK.h
+++ Inverse Kinematics/Inverse Kinematics/IK.h
@@ -6,10 +6,11 @@ class IK
 {
 public:
 	IK(void);
-	IK(const Point& a_point);
+	IK(const Point& a_point, double a, double b);
 	~IK(void);
 	vector<double> getAngles() const;
 private:
+	double px,py,l1,l2;
 	Point m_point;
 	vector<double> m_angles;
 };
